10eb97b9a5217e5c35d20dc33edd5069d8ca3d0b
Evgeniy <87270911+evgenyBukharin@users.noreply.github.com>
2023-06-27 11:47:05 +0500

    768+ adaptive

--- src/js/components/articles-count.js
+++ src/js/components/articles-count.js
@@ -1,4 +1,8 @@
 const columns = document.querySelectorAll(".activity__column");
 columns.forEach((column) => {
+	if (column.classList.contains("activity__column-cases")) {
+		column.setAttribute("data-count", column.querySelectorAll(".activity__slide-actual").length);
+		return;
+	}
 	column.setAttribute("data-count", column.querySelectorAll(".swiper-slide").length);
 });
--- src/js/components/custom-scroll.js
+++ src/js/components/custom-scroll.js
@@ -17,6 +17,9 @@ const swiper = new Swiper(document.querySelector(".hero__slider"), {
 		769: {
 			slidesPerView: 4,
 		},
+		481: {
+			slidesPerView: 6,
+		},
 	},
 });
 
@@ -31,8 +34,15 @@ const swiper1 = new Swiper(document.querySelector(".activity__slider-1"), {
 		1151: {
 			slidesPerView: 7,
 		},
-		769: {
+		921: {
 			slidesPerView: 6,
+		},
+		752: {
+			slidesPerView: 5,
+		},
+		600: {
+			slidesPerView: 4,
+			spaceBetween: 25,
 		},
 	},
 });
@@ -41,10 +51,18 @@ const swiper2 = new Swiper(document.querySelector(".activity__slider-2"), {
 	slidesPerView: 2,
 	spaceBetween: 25,
 	mousewheel: true,
-	direction: "vertical",
 	scrollbar: {
 		el: ".activity__scrollbar-2",
 		draggable: true,
+	},
+	breakpoints: {
+		921: {
+			direction: "vertical",
+		},
+		0: {
+			spaceBetween: 38,
+			direction: "horizontal",
+		},
 	},
 });
 
@@ -52,10 +70,18 @@ const swiper3 = new Swiper(document.querySelector(".activity__slider-3"), {
 	slidesPerView: 2,
 	spaceBetween: 25,
 	mousewheel: true,
-	direction: "vertical",
 	scrollbar: {
 		el: ".activity__scrollbar-3",
 		draggable: true,
+	},
+	breakpoints: {
+		921: {
+			direction: "vertical",
+		},
+		0: {
+			spaceBetween: 38,
+			direction: "horizontal",
+		},
 	},
 });
 
