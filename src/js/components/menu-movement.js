eedbcb6404145f3b3e9d1f4b4e48e7b9fce27265
Evgeniy <87270911+evgenyBukharin@users.noreply.github.com>
2023-06-28 10:34:46 +0500

    menu fix

--- src/js/components/menu-movement.js
+++ src/js/components/menu-movement.js
@@ -1,7 +1,15 @@
 const menu = document.querySelector(".menu");
 const burger = document.querySelector(".burger");
+const body = document.querySelector(".page__body");
 
 burger.addEventListener("click", () => {
 	menu.classList.toggle("menu-active");
 	burger.classList.toggle("burger--active");
+	if (body.style.overflowY == "hidden") {
+		body.style.overflowY = "unset";
+		body.style.paddingRight = "0";
+	} else {
+		body.style.paddingRight = window.innerWidth - body.offsetWidth + "px";
+		body.style.overflowY = "hidden";
+	}
 });
